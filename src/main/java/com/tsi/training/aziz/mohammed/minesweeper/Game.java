d0d6cf6e88acf6725131954dd967dcc213ee1fbd
naimz07 <94609897+naimz07@users.noreply.github.com>
2021-11-23 09:16:04 +0000

    edited code

--- src/main/java/com/tsi/training/aziz/mohammed/minesweeper/Game.java
+++ src/main/java/com/tsi/training/aziz/mohammed/minesweeper/Game.java
@@ -2,61 +2,32 @@ package com.tsi.training.aziz.mohammed.minesweeper;
 
 import java.util.Scanner;
 
-/**
- * Minesweeper
- *
- * Class Game:
- * Text based implementation of the game with a game loop.
- * The game ends when either all non-bomb cells have been successfully
- * revealed or when the player loses by revealing a bomb.
- *
- */
-public class Game extends Board {
-    /**
-     * Reference to the board object representing the game's state.
-     */
-    private final Board board;
-    /**
-     * Reference to a shared Scanner object.
-     */
+public class Game {
     private Scanner scan;
+    private Board board;
 
-    /**
-     * Initialises the game with a shared Scanner.
-     * Creates a board with size 10,10 and spawns up to 10 bombs on it.
-     */
     public Game() {
         scan = new Scanner(System.in);
         board = new Board(10,10);
         board.spawnBombs(10);
     }
 
-    /**
-     * Handles the game loop swapping between printing the board and
-     * handling input to reach the next state.
-     * If a position is typed in followed by "flag" the target cell will be flagged.
-     * Otherwise the position will be used to reveal that target cell.
-     * Once the game reaches either a won state where all non-bombs
-     * have been revealed or a bomb has been revealed the game ends.
-     * When the game ends the full board is revealed and a victory/defeat message is shown.
-     */
     public void startGame() {
-        // Used to apply commands in addition to the coordinate.
-        String extraCommand;
+        boolean isFlagging;
         Position inputPosition;
         do {
             board.printBoard();
             board.printStatus();
             inputPosition = getPositionInput();
-            extraCommand = getStringOrQuit(scan).trim();
-            if(extraCommand.equalsIgnoreCase("flag")) {
+            isFlagging = getStringOrQuit().equalsIgnoreCase("flag");
+            if(isFlagging) {
                 board.flagCell(inputPosition);
             } else if(board.isCellFlagged(inputPosition)) {
                 System.out.println("You need to un-flag that cell first.");
             } else {
                 board.revealCell(inputPosition);
             }
-        } while(!board.isWon() && (extraCommand.equalsIgnoreCase("flag") || !board.isCellBomb(inputPosition)));
+        } while(!board.isWon() && (isFlagging || !board.isCellBomb(inputPosition)));
         board.revealAll();
         board.printBoard();
         if(board.isWon()) {
@@ -66,44 +37,8 @@ public class Game extends Board {
         }
     }
 
-    /**
-     * Continues looping until a valid pair of integers have been entered.
-     * Each of the two inputs must be valid and must be inside the play space on the board.
-     * The values are stored into targetX and targetY with adjusted values
-     * to take them from the 1 to n range to the 0 to n-1 range.
-     */
-    public Position getPositionInput() {
-        Position input = new Position(0,0);
-        do {
-            System.out.println("Enter space separated X (bottom) then Y (right) coordinate: ");
-            if(!scan.hasNextInt()) {
-                getStringOrQuit(scan);
-                System.out.println("Invalid X coordinate.");
-                continue;
-            }
-            input.x = scan.nextInt();
-            if(!scan.hasNextInt()) {
-                getStringOrQuit(scan);
-                System.out.println("Invalid Y coordinate.");
-                continue;
-            }
-            input.y = scan.nextInt();
-            input.x--;
-            input.y--;
-        } while(!isPositionInputValid(input));
-        return input;
-    }
-
-    /**
-     * Tests if the specified x and y coordinate are both inside the valid play space.
-     * Then also checks if the cell has already been revealed. Can not reveal a cell that
-     * has already been revealed.
-     *
-     * @param position The position with an x and y coordinate.
-     * @return True if the coordinate is inside the play space and the cell has not been revealed.
-     */
-    private boolean isPositionInputValid(Position position) {
-        if(!board.validPosition(position)) {
+    public boolean isPositionInputValid(Position position) {
+        if (!board.validPosition(position)) {
             System.out.println("Coordinate not inside the play space!");
             return false;
         }
@@ -114,17 +49,32 @@ public class Game extends Board {
         return true;
     }
 
-    /**
-     * Helper method that reads in a line of text and checks if the text is "quit".
-     * If the text is "quit" it will exit the game. Otherwise returns the input.
-     *
-     * @param scan Reference to shared Scanner object.
-     * @return The input from nextLine() if the input was not "quit".
-     */
-    public static String getStringOrQuit(Scanner scan) {
-        String input = scan.nextLine();
+    public Position getPositionInput() {
+        Position input = new Position(0,0);
+        do {
+            System.out.println("Enter space separated X (bottom) then Y (right) coordinate: ");
+            if(!scan.hasNextInt()) {
+                getStringOrQuit();
+                System.out.println("Invalid X coordinate.");
+                continue;
+            }
+            input.x = scan.nextInt();
+            if(!scan.hasNextInt()) {
+                getStringOrQuit();
+                System.out.println("Invalid Y coordinate.");
+                continue;
+            }
+            input.y = scan.nextInt();
+            input.x--;
+            input.y--;
+        } while(!isPositionInputValid(input));
+        return input;
+    }
+
+    public String getStringOrQuit() {
+        String input = scan.nextLine().trim();
         if(input.equalsIgnoreCase("quit")) {
-            System.out.println("Thanks for playing! Goodbye!");
+            System.out.println("Thanks for playing!");
             System.exit(0);
         }
         return input;
--- src/test/java/com/tsi/test/aziz/mohammed/mindsweeper/BoardTest.java
+++ src/test/java/com/tsi/test/aziz/mohammed/mindsweeper/BoardTest.java
@@ -1,30 +1,61 @@
 package com.tsi.test.aziz.mohammed.mindsweeper;
 
 import com.tsi.training.aziz.mohammed.minesweeper.Board;
+import com.tsi.training.aziz.mohammed.minesweeper.Position;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class BoardTest {
 
     @Test
-    void testPrintBoard(){
-        Board testBoard = new Board();
-        testBoard.printBoard();
-        System.out.println("testPrintBoard has passed");
+    public void testValidPosition() {
+        Board testBoard = new Board(10,10);
+        Position position = new Position(9, 9);
+        boolean isValidPosition = testBoard.validPosition(position);
+        assertEquals(true, isValidPosition, "True");
     }
 
     @Test
-    void testPrintStatus(){
-        Board testBoard = new Board();
-        testBoard.printBoard();
-        System.out.println("testPrintStatus has passed");
+    public void testRevealcell() {
+        Board testBoard = new Board(10, 10);
+        Position position = new Position(9, 9);
+        testBoard.revealCell(position);
+        int revealedTotal = testBoard.revealedTotal;
+        assertEquals(100, revealedTotal);
+
+
     }
 
+    @Test
+    public void testIsCellRevealed() {
+        Board testBoard = new Board(10, 10);
+        Position position = new Position(5, 5);
+        boolean isCellRevealed = testBoard.isCellRevealed(position);
+        assertEquals(false, isCellRevealed, "False");
+    }
+
+
+//        @Test
+//        void testPrintBoard(){
+//            Board testBoard = new Board();
+//            testBoard.printBoard();
+//            assert();
+//
+//        }
+//
 //    @Test
-//    void testRevealAll(){
+//    void testPrintStatus(){
 //        Board testBoard = new Board();
-//        testBoard.revealAll();
+//        testBoard.printBoard();
+//        assertEquals();
+//    }
+//
+//    @Test
+//    void testRevealCell(){
+//        Board testBoard = new Board();
+//        testBoard.revealCell(10);
 //        System.out.println("testRevealAll has passed");
 //    }
 //
@@ -36,8 +67,8 @@ class BoardTest {
 //        testBoard.spawnBombs(10);
 //        assertEquals(10, 10,"testSpawnBomb has passed");
 //    }
-
-
-
-
 }
+
+
+
+
