5b51f0bbbd103df5a443bff6737200369aebdcfe
naimz07 <94609897+naimz07@users.noreply.github.com>
2021-11-23 11:16:20 +0000

    test report 3

--- src/test/java/com/tsi/test/aziz/mohammed/mindsweeper/BoardTest.java
+++ src/test/java/com/tsi/test/aziz/mohammed/mindsweeper/BoardTest.java
@@ -1,8 +1,11 @@
 package com.tsi.test.aziz.mohammed.mindsweeper;
 
 import com.tsi.training.aziz.mohammed.minesweeper.Board;
+import com.tsi.training.aziz.mohammed.minesweeper.Cell;
 import com.tsi.training.aziz.mohammed.minesweeper.Position;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -18,16 +21,30 @@ public class BoardTest {
         Position position2 = new Position(11, 9);
         boolean isValidPosition2 = testBoard.validPosition(position2);
         assertEquals(false, isValidPosition2, "False");
+        System.out.println("validPosition test complete, test Passed");
     }
 
     @Test
-    public void testRevealTotal() {
+    public void testRevealCell() {
         Board testBoard = new Board(10, 10);
         Position position = new Position(9, 9);
         testBoard.revealCell(position);
         int revealedTotal = testBoard.revealedTotal;
         assertEquals(100, revealedTotal);
+        System.out.println("revealCell test complete, test Passed");
     }
+
+//    public void revealCell(Position position) {
+//        if(cells[position.x][position.y].getNeighbours() != 0) {
+//            revealedTotal++;
+//            cells[position.x][position.y].reveal();
+//        } else {
+//            List<Position> revealedCells = floodFillReveal(position);
+//            for(Position p : revealedCells) {
+//                revealAllAroundPoint(p);
+//            }
+//        }
+//    }
 
     @Test
     public void testIsCellRevealed() {
@@ -35,7 +52,7 @@ public class BoardTest {
         Position position = new Position(5, 5);
         boolean isCellRevealed = testBoard.isCellRevealed(position);
         assertFalse(isCellRevealed, "False");
-        System.out.println("isCellRevealed test works");
+        System.out.println("isCellRevealed test complete, test Passed");
     }
 
     @Test
@@ -44,8 +61,34 @@ public class BoardTest {
         Position position = new Position(5, 5);
         boolean isCellBomb = testBoard.isCellBomb(position);
         assertEquals(false, isCellBomb, "False");
+        System.out.println("isCellBomb test complete, test Passed");
+
     }
 
+    @Test
+    public void testIsCellFlagged(){
+        Board testBoard = new Board(10, 10);
+        Position position = new Position(5, 5);
+        boolean isCellFlagged = testBoard.isCellFlagged(position);
+        assertEquals(false,isCellFlagged, "false");
+        System.out.println("isCellFlagged test complete, test Passed");
+    }
+
+    @Test
+    public void testFlagCell(){
+        Board testBoard = new Board(10, 10);
+        Position position = new Position(5, 5);
+        testBoard.flagCell(position);
+        assertEquals(position.isFlagged, testBoard.isFlagged, "is flagged");
+        System.out.println("flagCell test complete, test Passed");
+    }
+
+
+
+//    public void flagCell(Position position) {
+//        cells[position.x][position.y].toggleIsFlagged();
+//    }
+//
 
 
 
